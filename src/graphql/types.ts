21ac58aff3aab99a7adb68a26d5d88acf93f8ac0
Pras, Manuel <Manuel.Pras@zuehlke.com>
2017-08-25 21:52:21 +0200

    return id of debtor and creditor instead of user object

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -27,8 +27,8 @@ export const Debt = new GraphQLObjectType({
     description: "This represents a debt. A debt always contains a debtor and a creditor (both are users)",
     fields: () => ({
         _id: { type: new GraphQLNonNull(GraphQLString) },
-        debtor: { type: new GraphQLNonNull(User) },
-        creditor: { type: new GraphQLNonNull(User) },
+        debtorId: { type: new GraphQLNonNull(GraphQLString) },
+        creditorId: { type: new GraphQLNonNull(GraphQLString) },
         timestamp: { type: new GraphQLNonNull(GraphQLFloat) },
         reason: { type: new GraphQLNonNull(GraphQLString) },
         amount: { type: new GraphQLNonNull(GraphQLFloat) }
